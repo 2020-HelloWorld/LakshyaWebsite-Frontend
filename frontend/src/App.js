fe253938ee0c083e8ae7028f07b9c082e9838db4
Shreya R <109687884+Shreya-R-2003@users.noreply.github.com>
2023-07-05 19:35:34 +0530

    candidate home page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import Candidates from './Candidates';
 
 
 import {Header,Footer} from './components/layout';
 import  ReloadOnBackButton from "./ReloadOnBackButton";
 
 
-const Home = () => {
-  console.log("Home")
-  return <h2>Home Page</h2>;
-};
+// const Home = () => {
+//   console.log("Home")
+//   return <h2>Home Page</h2>;
+// };
 
 
-const Profile = () => {
-  return <h2>Profile Page</h2>;
-};
+// const Profile = () => {
+//   return <h2>Profile Page</h2>;
+// };
 
-const Logout = () => {
-  return <h2>Logout Page</h2>;
-};
+// const Logout = () => {
+//   return <h2>Logout Page</h2>;
+// };
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        
-        <Switch>
+        {/* <Header/> */}
+        <Candidates />
+        {/* <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/profile" component={Profile} />
           <Route path="/logout" component={Logout} />
-        </Switch>
+        </Switch> */}
 
         <ReloadOnBackButton />
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
