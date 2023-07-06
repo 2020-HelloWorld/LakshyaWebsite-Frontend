259619a5af77670ead2f185c25eb4648bdc42f3a
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-07-06 17:58:52 +0530

    Updated App.js with /mentor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ReloadOnBackButton from './ReloadOnBackButton';
-import { AboutUs,Companies,Hero ,mentorHome,Testimonial,Mentor} from './components/pages';
-import {Header,Footer} from './components/layout';
-
-
+import { MentorHome, Testimonial, Mentor, Companies } from './components/pages';
+import { Header, Footer } from './components/layout';
 
 const Home = () => {
   console.log("Home")
   return <h2>Home Page</h2>;
 };
-
 
 const Profile = () => {
   return <h2>Profile Page</h2>;
@@ -20,22 +17,32 @@ const Logout = () => {
   return <h2>Logout Page</h2>;
 };
 
+const MentorPage = () => {
+  return (
+    <>
+      <MentorHome />
+      <Companies />
+      <Mentor />
+      <Testimonial />
+      
+    </>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-        <Header/>
-        
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/mentor" component={mentorHome} />
-        </Switch>
-<Companies/>
-<Mentor/>
-       <ReloadOnBackButton/>
-       <Testimonial/>
-      <Footer/>
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/mentor" component={MentorPage} />
+      </Switch>
+
+      <ReloadOnBackButton />
+      <Footer />
     </div>
   );
 }
