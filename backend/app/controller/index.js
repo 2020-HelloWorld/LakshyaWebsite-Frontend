66117073ef60cdbedf5f190e85c3f6c131f07b9c
Ghanashyam Bhat <ghanashyambhat6@gmail.com>
2023-07-10 22:40:47 +0530

    frontend

--- backend/app/controller/index.js
+++
@@ -1,9 +0,0 @@
-const loginController = require("./loginController");
-const homeController = require("./homeController");
-const signupController = require("./signupController");
-
-module.exports = {
-    loginController,
-    homeController,
-    signupController,
-}
