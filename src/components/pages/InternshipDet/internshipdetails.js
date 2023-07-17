d7f1c0b9e76b1affcc82be0fa59215715b178e4b
Shreeya Rao <shreeyasatishrao@gmail.com>
2023-07-17 13:14:19 +0530

    Internship Details Page

---
+++ src/components/pages/InternshipDet/internshipdetails.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar ,Footer} from '../../layout';
+// import Main from "./internshipdet";
+import ApplyNowStates from './applynowstates';
+const Internshipdetails = () => {
+  return (
+    <>
+    <Navbar />
+     <ApplyNowStates/>
+    <Footer />
+    </>
+  )
+}
+
+export default Internshipdetails
