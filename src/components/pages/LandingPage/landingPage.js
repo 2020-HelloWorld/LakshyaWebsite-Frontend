82c2304066cfda2ae802d10f38345762c646671e
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-07-18 18:27:49 +0530

    Updated landingPage.js

--- src/components/pages/LandingPage/landingPage.js
+++ src/components/pages/LandingPage/landingPage.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import './LandingPage.css'
+
 
 import { Navbar ,Footer} from '../../layout';
 
 import IntoScreen from "./IntroScreen/IntroScreen";
-import AboutUs from './aboutus/aboutus';
+
 import Companies from './companies/companies';
 import UserType from "./UserType/UserType";
-import Value from "./value/value";
+
 
 function LandingPage() {
   return (
     <div>
-      <div className="landing-page">
+      <div>
       <Navbar />
       <IntoScreen />
       <UserType />
       <Companies />
-      <Value />
-      <AboutUs />
       <Footer />
     </div>
     </div>
