93359fd5559580a8d9f992fef2b85691aed4cd55
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-07-18 12:42:53 +0530

    Updated index.js with signup

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -5,6 +5,7 @@ export {default as CandidateHome} from "./Home/candidate/candidateHome";
 export {default as MentorHome} from "./Home/mentor/mentorHome";
 
 export {default as Login} from "./Login/login"
+export {default as SignUp} from "./Signup/signup"
 
 export {default as jobList} from "./jobs/jobList/jobList";
 export {default as jobDescription} from "./jobs/jobDescription/jobDescription";
