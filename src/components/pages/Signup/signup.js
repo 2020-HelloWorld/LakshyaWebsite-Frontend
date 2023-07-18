714e4dadccc4fba24675bf42b51b9a253a2e6c96
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-07-18 12:41:49 +0530

    Updated signup.js

--- src/components/pages/Signup/signup.js
+++ src/components/pages/Signup/signup.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import CandidateSignUp from './candidatesignup';
+import CommonSignUp from './CommonSignUp'
 
 const SignUp = () => {
   return (
     <div>
-      <h2>Candidate Signup</h2>
-      <CandidateSignUp type={1} />
-
-      <h2>Company Signup</h2>
-      <CandidateSignUp type={2} />
-
-      <h2>Mentor Signup</h2>
-      <CandidateSignUp type={3} />
+      
+      <CommonSignUp/>
     </div>
   );
 };
