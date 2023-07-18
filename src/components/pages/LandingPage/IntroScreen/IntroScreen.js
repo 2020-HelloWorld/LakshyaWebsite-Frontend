a11c05e769170c907d560d79b4a791c192a7bbaf
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-07-18 18:29:22 +0530

    Updated IntroScreen.js with tailwind css

--- src/components/pages/LandingPage/IntroScreen/IntroScreen.js
+++ src/components/pages/LandingPage/IntroScreen/IntroScreen.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import "./IntroScreen.css";
-import introimg from '../../../../static/design/introimg.png';
+import introimg from '../../../../static/design/Intro.png';
 
-const IntoScreen = () => {
+const IntroScreen = () => {
   return (
-    <section className="intoduction-wrapper">
-      <div className="paddings  margin intoduction-container">
-        <div className="flexColStart intoduction-left">
-          <div className="intoduction-title">
-            <h1>Empowering Women,</h1>    
-            <h1>Shaping Careers,</h1> 
-            <h1>Building Future</h1>
+    <section className="bg-gray-100">
+      <div className="p-8 md:p-16 lg:p-24 flex flex-col md:flex-row items-center">
+        <div className="w-full md:w-1/2">
+          <div>
+            <h1  style={{ fontSize: '3rem' }} className="text-white text-4x1 md:text-5x1 lg:text-6x1 font-oxygen leading-tight mb-2">
+              Empowering Women,
+            </h1>
+            <h1    style={{ fontSize: '3rem' }} className="text-white text-4x1 md:text-4x1 lg:text-5x1 font-oxygen leading-tight mb-2">
+              Shaping Careers,
+            </h1>
+            <h1  style={{ fontSize: '3rem' }} className="text-white text-4x1 md:text-4x1 lg:text-5x1 font-oxygen leading-tight mb-2">
+              Building Future
+            </h1>
           </div>
-          <div className="flexColStart intoduction-des">
-            <span>Unlocking Potential, Inspiring Careers: Empowering Women<br />  to Thrive in the Workforce and Beyond</span>       
+          <div>
+            <span className="text-white text-2xl font-oxygen md:text-lg lg:text-xl leading-7">
+              Unlocking Potential, Inspiring Careers: Empowering Women to Thrive in the Workforce and Beyond
+            </span>
           </div>
         </div>
-        <div className=" intoduction-right">
-           <div className="image-container"> 
-            <img src={introimg} alt="" />
-          </div> 
+        <div className="w-full md:w-1/2 flex justify-end">
+          <div className="hidden md:block pl-10">
+            <img src={introimg} alt="" className="max-w-full w-110 h-96 rounded-md" />
+          </div>
         </div>
       </div>
     </section>
   );
 };
 
-export default IntoScreen;
+export default IntroScreen;
+
