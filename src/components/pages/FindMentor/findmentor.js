425ddf679230457b43daa1a26a9aa162ee24f3b3
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-07-24 20:03:58 +0530

    Created findmentor.js

---
+++ src/components/pages/FindMentor/findmentor.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MentorList from './MentorList';
+import NavBar from '../../layout/Navbar/NavBar';
+import Footer from '../../layout/footer/footer';
+const Mentor = () => {
+  return (
+    <div className="pt-8">
+      <NavBar />
+    <MentorList />
+        <Footer/>
+    </div>
+  );
+};
+
+export default Mentor;
