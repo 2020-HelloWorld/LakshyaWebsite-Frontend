86c55c262852cd9bea2cb072b614430f87452567
Jeevanjr07 <jeevan102002@gmail.com>
2023-07-25 23:26:24 +0530

    event list functionality done
    event list functionality done
    events are retrieved from backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://f029-2401-4900-330c-a395-ccc6-2e96-14a6-3aec.ngrok-free.app',
+      target: 'https://7c91-2405-201-d031-6835-46c-10ed-194d-897a.ngrok-free.app',
       changeOrigin: true,
     })
   );
